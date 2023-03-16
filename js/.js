47efb944873711f9fb56fa786aff4873271819d8
Sunil Sharma <sunilsharma7583@gmail.com>
2023-03-16 10:35:24 +0530

    Html layout

---
+++ js/.js
@@ -0,0 +1,13 @@
+$(document).ready(function () {
+    $(".slider").slick({
+      infinite: true,
+      slidesToShow: 1,
+      slidesToScroll: 1,
+      autoplay: true,
+      autoplaySpeed: 1300,
+      arrows: false,
+         dots: false,
+    });
+  });
+  // 
+  
